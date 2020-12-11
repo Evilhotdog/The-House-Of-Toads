2a9349b2b9ecdd0ce10e3beb068c342a59f76876
Evilhotdog <rubenaharris05@gmail.com>
2020-12-11 19:01:56 +0000

    Updated styling of comment

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-
+class Comment extends React.Component{
+    render() {
+        return <div class="comment">
+            <h5 class="commentname">{this.props.username}</h5> <span class="commentdate">{this.props.date}</span>
+            <p class="commenttext">{this.props.text}</p>
+        </div>
+        
+    }
+}
 console.log("Hello")
-ReactDOM.render(<h2>Hello</h2>, document.getElementById('commentsList'))
+ReactDOM.render(<Comment username="Ryan"text="hi mum" date="11th Nov 2020"/>, document.getElementById('commentsList'))
